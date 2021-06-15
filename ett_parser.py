508c0b696382f0336f522a909ebec0d9daf94d36
souzatharsis <souza.tharsis@gmail.com>
2021-06-15 09:11:48 -0400

    refactor code and add ett_parser.py

---
+++ ett_parser.py
@@ -0,0 +1,33 @@
+# ett_parser.py
+
+import requests
+
+
+def __url(path, legacy_API=False) -> str:
+    return ('https://www.elevenvr.club/' if legacy_API else 'https://elevenvr.club/api/v1/') + path
+
+
+def get_user(user_id) -> List:
+    return requests.get(_url(f'accounts/{user_id}/', legacy_API=True)).json()['data']['attributes']
+
+
+def get_friends(user_id) -> List:
+    return requests.get(_url(f'accounts/{user_id}/', legacy_API=True) + "friends").json()['data']
+
+
+def get_matches(user_id) -> List:
+    return requests.get(_url(f'accounts/{user_id}/matches')).json()['data']
+
+
+def get_elo_history(user_id) -> List:
+    return requests.get(_url(f'accounts/{user_id}/', legacy_API=True) + 'elo-history').json()['data']
+
+
+def get_leaderboard() -> List:
+    return requests.get(_url(f'leaderboards/', legacy_API=True)).json()['scores']
+
+
+def user_search(username) -> List:
+    return requests.get(_url(f'accounts/search/{username}/', legacy_API=True)).json()['data']
+
+
