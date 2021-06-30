55e539214ff1493aa97555b03793a413ce5653f8
souzatharsis <souza.tharsis@gmail.com>
2021-06-30 00:51:12 -0400

    update docs

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("LICENSE.md") as f:
 
 setup(
     name="pyETT",
-    version="0.1.7",
+    version="0.1.8",
     description="Python library for Eleven VR Table Tennis data",
     long_description=readme,
     long_description_content_type="text/markdown",
